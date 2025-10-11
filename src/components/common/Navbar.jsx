251785e7369549cb15577bfce326f58c18bca9a6
Nirmal <nirmalsep2001@gmail.com>
2025-10-11 14:57:01 +0530

    Small Changes

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
 
       {/* Logout Confirmation Modal */}
       {showLogoutModal && (
-        <div className="fixed inset-0  bg-opacity-50 flex items-center justify-center z-50 p-4">
+        <div className="fixed inset-0 bg-black/30 bg-opacity-70 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-lg shadow-2xl max-w-md w-full p-6 relative animate-fade-in">
             <button
               onClick={cancelLogout}
--- src/components/common/SidePannel.jsx
+++ src/components/common/SidePannel.jsx
@@ -124,7 +124,7 @@ const SettingIcon = (
 
       {/* Logout Confirmation Modal */}
       {showLogoutPopup && (
-         <div className="fixed inset-0  bg-opacity-50 flex items-center justify-center z-50 p-4">
+         <div className="fixed inset-0 bg-black/30 bg-opacity-50 flex items-center justify-center z-[99999] p-4">
           <div className="bg-white rounded-lg shadow-2xl max-w-md w-full p-6 relative animate-fade-in">
             <button
               onClick={cancelLogout}
