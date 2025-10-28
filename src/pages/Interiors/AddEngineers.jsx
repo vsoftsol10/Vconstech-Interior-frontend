55bac2b5ba8258ea6788c3ea4207aa596e169737
vigneshwaran <vigneshwaran274mail@gmail.com>
2025-10-28 11:47:09 +0530

    commit

--- src/pages/Interiors/AddEngineers.jsx
+++ src/pages/Interiors/AddEngineers.jsx
@@ -484,13 +484,13 @@ const AddEngineers = () => {
                   </div>
 
                   {/* Phone */}
-                  <div>
+                  <div>    
                     <label className="block text-sm font-medium text-gray-700 mb-2">
                       Phone Number <span className="text-red-500">*</span>
                     </label>
                     <div className="relative">
                       <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <Phone className="h-5 w-5 text-gray-400" />
+                        <Phone className="h-5 w-5 text-gray-400" />          
                       </div>
                       <input
                         type="tel"
